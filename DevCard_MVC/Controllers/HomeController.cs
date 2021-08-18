a971ee0003994b48a5a71579dd48acb1fb7896c9
zande <zande2025@gmail.com>
2021-08-18 19:24:02 +0430

    use IFormCollection in Form posted

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
 
 namespace DevCard_MVC.Controllers
 {
@@ -11,15 +12,23 @@ namespace DevCard_MVC.Controllers
             return View();
         }
 
+
+        [HttpGet]
         public IActionResult Contact()
         {
             return View();
         }
 
+        [HttpPost]
+        public void Contact(IFormCollection form)
+        {
+            string Name = form["name"];
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
     }
 }
---
+++ DevCard_MVC/Controllers/test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.Controllers
+{
+    public class Test:Controller
+    {
+        public FileResult Index()
+        {
+            var fileB = System.IO.File.ReadAllBytes("wwwroot/TextFile.txt");
+            const string filename = "file.txt";
+            return File(fileB, MediaTypeNames.Text.Plain, filename);
+        }
+    }
+}
